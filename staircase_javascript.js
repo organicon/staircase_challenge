1ebbb43d9f5053a87aa3d93ec5622010378f7bdb
Mark Pettit <mark.pettit.default@gmail.com>
2025-03-10 15:33:14 -0400

    added js file;

---
+++ staircase_javascript.js
@@ -0,0 +1,10 @@
+const count_ways(n) => {
+    if (n === 0) return 1;
+    if (n === 1 || n === 2) return n;
+    let dp = new Array(n + 1).fill(0);
+    for (i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+      }
+    
+      return dp[n];
+}
