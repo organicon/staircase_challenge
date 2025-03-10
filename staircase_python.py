b6d1bd6bac91bb03f1a0193e1159341775085c35
organicon <organicon@hotmail.com>
2025-03-10 14:50:52 -0400

    Create staircase_python.py

---
+++ staircase_python.py
@@ -0,0 +1,15 @@
+def count_ways(n: int) -> int:
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+
+    dp = [0] * (n + 1)
+    dp[0], dp[1], dp[2] = 1, 1, 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    return dp[n]
